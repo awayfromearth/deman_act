ffcda740af51b420ac4ffbdbd947d976b7806d0d
awayfromearth <demanchenmian@163.com>
2024-06-06 09:50:45 +0800

    dev

--- src/plugins/request.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const request = axios.create({
-  baseURL: 'https://ygk.nbdeman.com:11112/api',
-  timeout: 180000,
-  withCredentials: false
-})
-
-export default request
